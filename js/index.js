a856d15de3eca7320d800a56972ac74764c17cea
estiakahmed <71464257+estiakahmed@users.noreply.github.com>
2022-07-07 21:25:55 +0600

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,19 @@
+// 
+$(document).ready(function () {
+    //toggle the component with class accordion_body
+    $(".accordion_head").click(function () {
+        if ($(".accordion_head").css("background-color", "")) {
+            $('.accordion_body').is(':visible');
+            $(".accordion_body").slideUp(300);
+            $(".plusminus").text('+');
+        }
+        if ($(this).next(".accordion_body").is(':visible')) {
+            $(this).next(".accordion_body").slideUp(300);
+            $(this).children(".plusminus").text('+');
+        } else {
+          $(".accordion_head").css("background-color", "")
+            $(this).next(".accordion_body").slideDown(300);
+            $(this).children(".plusminus").text('-');
+        }
+    });
+});
